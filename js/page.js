7e7b3c217beb72e7324dd5995c19b1ec3062822b
benkovra <112646826+benkovra@users.noreply.github.com>
2022-11-08 11:37:28 -0500

    nav-wrap

--- js/page.js
+++ js/page.js
@@ -1,4 +1,15 @@
-// a test function that runs when the button is clicked
-function sampleFunction() {
-  alert("You clicked the button");
-}
+
+Function navToggle() {
+  Let siteNav = document.getElementById(‘site-nav’);
+  Let trigram = document.getElementById(‘trigram’);
+  If (siteNav.className === ‘nav-wrap’) {
+    //then execute
+    siteNav.classList.add(‘mobile’);
+    trigram.classList.add('tri-mobile');
+  }
+  else {
+  siteNav.classList = (‘nav-wrap’);
+  trigram.classList = (‘nav_link’);
+  }
+  }
+  
