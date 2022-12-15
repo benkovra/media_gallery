edeaa141f22244f3b773a3f774c4e780d5476873
benkovra <112646826+benkovra@users.noreply.github.com>
2022-12-14 19:40:34 -0500

    fixed js error

--- js/page.js
+++ js/page.js
@@ -1,15 +1,12 @@
 
-Function navToggle() {
-  Let siteNav = document.getElementById(‘site-nav’);
-  Let trigram = document.getElementById(‘trigram’);
-  If (siteNav.className === ‘nav-wrap’) {
-    //then execute
-    siteNav.classList.add(‘mobile’);
-    trigram.classList.add('tri-mobile');
-  }
-  else {
-  siteNav.classList = (‘nav-wrap’);
-  trigram.classList = (‘nav_link’);
-  }
-  }
-  
+  function navToggle() {
+    let siteNav = document.getElementById('site-nav');
+    let trigram = document.getElementById('trigram');
+    if (siteNav.className === 'nav-wrap') {
+      siteNav.classList.add('mobile');
+      trigram.classList.add('tri-mobile');
+    } else {
+      siteNav.classList = 'nav-wrap';
+      trigram.classList = 'nav-link';
+    }
+    }
